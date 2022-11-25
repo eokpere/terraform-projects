4f4752e66b8972f0ede1eaab0e75c1aee105d116
eokpere <evelynokpere@outlook.com>
2022-11-25 15:03:02 -0500

    creates asg

--- shebuild-lab/main.tf
+++ shebuild-lab/main.tf
@@ -37,34 +37,40 @@ module "security_group" {
 }
 
 module "ecs_task_execution_role" {
-  source ="../modules/ecs-tasks-execution-role"
-  project_name = module.vpc.project_name #because we created the project name in the vpc module
+  source        ="../modules/ecs-tasks-execution-role"
+  project_name  = module.vpc.project_name #because we created the project name in the vpc module
 }
 
 module "Aws-Cert-Mgr" {
-  source = "../modules/Aws-Cert-Mgr"
-  domain_name = var.domain_name
-  alternative_name = var.alternative_name
+  source              = "../modules/Aws-Cert-Mgr"
+  domain_name         = var.domain_name
+  alternative_name    = var.alternative_name
 }
 
 module "application_load_balancer" {
-  source = "../modules/alb"
-  project_name = module.vpc.project_name
+  source                = "../modules/alb"
+  project_name          = module.vpc.project_name
   alb_security_group_id = module.security_group.alb_security_group_id
-  public_subnet_az1_id = module.vpc.public_subnet_az1_id
-  public_subnet_az2_id = module.vpc.public_subnet_az2_id
-  vpc_id = module.vpc.vpc_id
-  certificate_arn = module.Aws-Cert-Mgr.certificate_arn
+  public_subnet_az1_id  = module.vpc.public_subnet_az1_id
+  public_subnet_az2_id  = module.vpc.public_subnet_az2_id
+  vpc_id                = module.vpc.vpc_id
+  certificate_arn       = module.Aws-Cert-Mgr.certificate_arn
 }
 
 module "ecs" {
-  source = "../modules/ecs"
-  project_name = module.vpc.project_name
-  ecs_task_execution_role_arn = module.ecs_task_execution_role.ecs_task_execution_role_arn
-  container_image = var.container_image
-  region = module.vpc.region
-  private_app_subnet_az1_id = module.vpc.private_app_subnet_az1_id
-  private_app_subnet_az2_id = module.vpc.private_app_subnet_az2_id
-  ecs_security_group_id = module.security_group.ecs_security_group_id
-  alb_target_group_arn = module.application_load_balancer.alb_target_group_arn
+  source                        = "../modules/ecs"
+  project_name                  = module.vpc.project_name
+  ecs_task_execution_role_arn   = module.ecs_task_execution_role.ecs_task_execution_role_arn
+  container_image               = var.container_image
+  region                        = module.vpc.region
+  private_app_subnet_az1_id     = module.vpc.private_app_subnet_az1_id
+  private_app_subnet_az2_id     = module.vpc.private_app_subnet_az2_id
+  ecs_security_group_id         = module.security_group.ecs_security_group_id
+  alb_target_group_arn          = module.application_load_balancer.alb_target_group_arn
+}
+
+module "auto_scaling_group" {
+  source              = "../modules/asg"
+  ecs_cluster_name    = module.ecs.ecs_cluster_name
+  ecs_service_name    = module.ecs.ecs_service_name
 }
